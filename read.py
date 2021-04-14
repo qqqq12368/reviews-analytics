86bb26481d72f24d92729dbe619959fce49936f0
Leon Lin <qqqq12368@gmail.com>
2021-04-14 23:53:15 +0800

    new filter

--- read.py
+++ read.py
@@ -14,10 +14,17 @@ for d in data:
 
 print('平均是', sum_len/len(data))
 
-
+# filter1
 new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('一共有', len(new), '比留言長度小於100')
 print(new[0])
+# filter 2
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '是good字串')
+print(good[0])
